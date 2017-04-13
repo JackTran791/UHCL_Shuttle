8aeca4646c70701de878918c3a18efa5fe2650e4
Jack Tran <jacktran@jacks-mbp.attlocal.net>
2017-04-13 15:02:17 -0500

    deploy

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 // var index = require('./routes/index');
-var users = require('./routes/users');
+// var users = require('./routes/users');
 var index = require('./routes/index');
-var postdata = require('./routes/postdata');
+// var postdata = require('./routes/postdata');
 
 var app = express();
 
@@ -25,9 +25,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // app.use('/index', index);
-app.use('/users', users);
+// app.use('/users', users);
 app.use('/', index);
-app.use('/postdata', postdata);
+// app.use('/postdata', postdata);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
